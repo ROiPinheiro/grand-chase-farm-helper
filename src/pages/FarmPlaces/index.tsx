cf66e1a403be5369ca25711c157a1f93ab64f7f0
roipinheiro <rafael_oliveira05@hotmail.com.br>
2023-08-27 12:29:45 -0300

    fix: fixed build errors

--- src/pages/FarmPlaces/index.tsx
+++ src/pages/FarmPlaces/index.tsx
@@ -1,102 +1,104 @@
-import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogCloseButton,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  Button,
-  Divider,
-  FormLabel,
-  Input,
-  useDisclosure,
-} from "@chakra-ui/react";
-import * as React from "react";
-import { Controller, useForm } from "react-hook-form";
-import PageContent from "../../components/PageContent";
-import PageHeader from "../../components/PageHeader";
-import { usePlacesToFarmStore } from "../../store/places-to-farm-store";
+export {};
 
-function FarmPlacesPage() {
-  const { placesToFarm, addPlaceToFarm } = usePlacesToFarmStore();
+// import {
+//   AlertDialog,
+//   AlertDialogBody,
+//   AlertDialogCloseButton,
+//   AlertDialogContent,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogOverlay,
+//   Button,
+//   Divider,
+//   FormLabel,
+//   Input,
+//   useDisclosure,
+// } from "@chakra-ui/react";
+// import * as React from "react";
+// import { Controller, useForm } from "react-hook-form";
+// import PageContent from "../../components/PageContent";
+// import PageHeader from "../../components/PageHeader";
+// import { usePlacesToFarmStore } from "../../store/PlacesToFarmStore";
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = React.useRef(null);
+function FarmPlacesPage() {}
+//   const { placesToFarm, addPlaceToFarm } = usePlacesToFarmStore();
 
-  const { control, handleSubmit, reset } = useForm({
-    defaultValues: {
-      placeToFarmName: "",
-    },
-  });
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const cancelRef = React.useRef(null);
 
-  function onSubmit(data: { placeToFarmName: string }) {
-    addPlaceToFarm(data.placeToFarmName);
-    onClose();
-  }
+//   const { control, handleSubmit, reset } = useForm({
+//     defaultValues: {
+//       placeToFarmName: "",
+//     },
+//   });
 
-  return (
-    <PageContent>
-      <PageHeader text="Farm places" />
+//   function onSubmit(data: { placeToFarmName: string }) {
+//     addPlaceToFarm(data.placeToFarmName);
+//     onClose();
+//   }
 
-      <Button onClick={onOpen}>Add new place to farm</Button>
+//   return (
+//     <PageContent>
+//       <PageHeader text="Farm places" />
 
-      <div>
-        {placesToFarm.map((place) => (
-          <div key={place.id}>{place.name}</div>
-        ))}
-      </div>
+//       <Button onClick={onOpen}>Add new place to farm</Button>
 
-      <AlertDialog
-        motionPreset="slideInBottom"
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-        onCloseComplete={reset}
-        isOpen={isOpen}
-        isCentered
-      >
-        <AlertDialogOverlay />
+//       <div>
+//         {placesToFarm.map((place) => (
+//           <div key={place.id}>{place.name}</div>
+//         ))}
+//       </div>
 
-        <AlertDialogContent>
-          <AlertDialogHeader>Add new place to farm</AlertDialogHeader>
-          <AlertDialogCloseButton />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <AlertDialogBody>
-              <span>Add here the name of place you want to farm</span>
-              <Divider className="pb-2" />
+//       <AlertDialog
+//         motionPreset="slideInBottom"
+//         leastDestructiveRef={cancelRef}
+//         onClose={onClose}
+//         onCloseComplete={reset}
+//         isOpen={isOpen}
+//         isCentered
+//       >
+//         <AlertDialogOverlay />
 
-              <Controller
-                name="placeToFarmName"
-                control={control}
-                render={({ field }) => (
-                  <>
-                    <FormLabel className="pt-2" htmlFor="input-name-place">
-                      Place name
-                    </FormLabel>
-                    <Input
-                      placeholder="Exemple: TOD 5F"
-                      id="input-name-place"
-                      name="input-name-place"
-                      onChange={field.onChange}
-                      defaultValue={field.value}
-                    />
-                  </>
-                )}
-              />
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button type="submit" colorScheme="red" ml={3}>
-                Add
-              </Button>
-            </AlertDialogFooter>
-          </form>
-        </AlertDialogContent>
-      </AlertDialog>
-    </PageContent>
-  );
-}
+//         <AlertDialogContent>
+//           <AlertDialogHeader>Add new place to farm</AlertDialogHeader>
+//           <AlertDialogCloseButton />
+//           <form onSubmit={handleSubmit(onSubmit)}>
+//             <AlertDialogBody>
+//               <span>Add here the name of place you want to farm</span>
+//               <Divider className="pb-2" />
+
+//               <Controller
+//                 name="placeToFarmName"
+//                 control={control}
+//                 render={({ field }) => (
+//                   <>
+//                     <FormLabel className="pt-2" htmlFor="input-name-place">
+//                       Place name
+//                     </FormLabel>
+//                     <Input
+//                       placeholder="Exemple: TOD 5F"
+//                       id="input-name-place"
+//                       name="input-name-place"
+//                       onChange={field.onChange}
+//                       defaultValue={field.value}
+//                     />
+//                   </>
+//                 )}
+//               />
+//             </AlertDialogBody>
+//             <AlertDialogFooter>
+//               <Button ref={cancelRef} onClick={onClose}>
+//                 Cancel
+//               </Button>
+//               <Button type="submit" colorScheme="red" ml={3}>
+//                 Add
+//               </Button>
+//             </AlertDialogFooter>
+//           </form>
+//         </AlertDialogContent>
+//       </AlertDialog>
+//     </PageContent>
+//   );
+// }
 
 export default FarmPlacesPage;
