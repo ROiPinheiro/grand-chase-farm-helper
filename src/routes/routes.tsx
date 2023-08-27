455731b5cf8857dbf6ee898edb7c9c257056a07f
roipinheiro <rafael_oliveira05@hotmail.com.br>
2023-08-27 12:31:41 -0300

    fix: remove unused route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -8,7 +8,7 @@ import CharacterSelectionPage from "../pages/CharacterSelection";
 import App from "../pages/App";
 import OptionsPage from "../pages/Options";
 import Home from "../pages/Home";
-import FarmPlacesPage from "../pages/FarmPlaces";
+// import FarmPlacesPage from "../pages/FarmPlaces";
 import HelperLinksPage from "../pages/HelperLinks";
 
 export const router = createBrowserRouter(
@@ -17,7 +17,7 @@ export const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="options" element={<OptionsPage />} />
       <Route path="character-selection" element={<CharacterSelectionPage />} />
-      <Route path="places" element={<FarmPlacesPage />} />
+      {/* <Route path="places" element={<FarmPlacesPage />} /> */}
       <Route path="helper-links" element={<HelperLinksPage />} />
     </Route>
   )
